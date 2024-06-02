3a98a836e19dfc006cd6da5cf25f6c08a2a56700
Nathan Brunelle <njb2b@virginia.edu>
2024-06-01 19:56:56 -0700

    autocommit caused by update

--- cal2html.py
+++ cal2html.py
@@ -53,7 +53,7 @@ def raw2cal(data, links=None):
         hasClass = True
         
         # handle metadata
-        if 'final' in data['meta'] and d == data['meta']['final']['start'].date() and d > e:
+        if 'final' in data['meta'] and d == data['meta']['final']['start'].date() and d > data['Special Dates']['Courses end']:
             final = data['meta']['final']
             ans.append({
                 "title":"Final Exam",
